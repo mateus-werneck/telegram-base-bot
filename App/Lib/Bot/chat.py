456c64fe7134bd8030f0f51d11cee9657efa3fa7
mateus-werneck <werneck.mateus@gmail.com>
2022-12-01 08:47:57 -0300

    Ajustes

--- App/Lib/Bot/chat.py
+++ App/Lib/Bot/chat.py
@@ -10,11 +10,11 @@ class BotChat(AbstractSingleton):
         self.bot_context = BotContext.instance()
 
     def send_text(self, message: str):
-        chat_id = self.get_chat_id()
+        chat_id = self.bot_context.get_chat_id()
         self.get_bot().send_message(chat_id, message)
 
     def send_callback_query(self, reply_markup: dict, text: str = ''):
-        chat_id = self.get_chat_id()
+        chat_id = self.bot_context.get_chat_id()
         self.get_bot().send_message(chat_id, text, reply_markup)
 
     def delete_message(self):
