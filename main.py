62588fa1aa11e7612f1f20633dab3ddf72f76303
mateus-werneck <werneck.mateus@gmail.com>
2022-12-01 08:34:47 -0300

    Ajustes

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from App.Lib.Bot.client import BotClient
+from App.Lib.Log.logger import Logger
 
-if __name__ == '__main__':
+
+def start_client():
     BotClient.instance()\
         .get_client()\
         .start_polling()
+    Logger.instance().info('[BotClient] Bot started successfully.')
+
+if __name__ == '__main__':
+    start_client()
